7031b03a077f3c25f288d88bce48600c9166b66d
LukasC3141 <lukascichy.zskom@seznam.cz>
2026-03-23 20:37:21 +0100

    Completed the Triangle method draw. Point x,y is the center of gravity of Triangle.

--- src/main/java/cz/uhk/vedit/gui/VeditFrame.java
+++ src/main/java/cz/uhk/vedit/gui/VeditFrame.java
@@ -2,6 +2,7 @@ package cz.uhk.vedit.gui;
 
 import cz.uhk.vedit.model.Circle;
 import cz.uhk.vedit.model.Square;
+import cz.uhk.vedit.model.Triangle;
 
 import javax.swing.*;
 import java.awt.*;
@@ -22,10 +23,11 @@ public class VeditFrame extends JFrame {
     }
 
     private void initSampleData() {
-        drawPanel.addObject(new Square(100,100, Color.RED, 50));
         drawPanel.addObject(new Square(200,200, Color.GREEN, 200));
         drawPanel.addObject(new Square(300,50, Color.RED, 30));
         drawPanel.addObject(new Square(150,220, Color.BLACK, 10));
         drawPanel.addObject(new Circle(200, 200, Color.BLUE, 20));
+        drawPanel.addObject(new Triangle(122, 122, Color.BLACK, 100));
+        drawPanel.addObject(new Circle(122, 122, Color.PINK, 20));
     }
 }
--- src/main/java/cz/uhk/vedit/model/Triangle.java
+++ src/main/java/cz/uhk/vedit/model/Triangle.java
@@ -3,6 +3,9 @@ package cz.uhk.vedit.model;
 import java.awt.*;
 
 public class Triangle extends AbstractGraphicObject{
+    /**
+     * length of the edge of a triangle
+     */
     protected int a;
 
     public Triangle(int a) {
@@ -22,7 +25,12 @@ public class Triangle extends AbstractGraphicObject{
     @Override
     public void draw(Graphics2D g) {
         g.setColor(color);
-        g.drawLine(point.x, point.y, point.x + a, point.y);
+        int h = (int)(this.a * Math.sin(Math.PI / 3));
+
+        g.drawLine(point.x - a/2, point.y  + h/3, point.x, point.y - 2*h/3);
+        g.drawLine(point.x, point.y - 2*h/3, point.x + a/2, point.y + h/3);
+        g.drawLine(point.x + a/2, point.y + h/3,point.x - a/2, point.y +h/3);
+
 
     }
 
